8d333ea6122473a4e92d9f6cf5eeced3aed823e0
Namrata Kasaraneni <44070869+nammyk789@users.noreply.github.com>
2022-05-05 11:47:50 -0700

    updates

--- kmeans.py
+++ kmeans.py
@@ -1,64 +1,86 @@
 import numpy as np
 from scipy.io import loadmat
-import time
+from scipy import stats as st
+
+class Hyperparameters:
+    def __init__(self, threshold=0.001):
+        self.threshold = threshold
 
 class kMeans:
-    def __init__(self, threshold = 0.1, num_centers=10):
-        self.num_centers = num_centers
+    def __init__(self, hyperparameters=None):
+        if not hyperparameters:
+            hyperparameters = Hyperparameters()
+        self.threshold = hyperparameters.threshold
+        self.num_centers = 10
 
-    def train(self, train_data, train_labels):
-        self.train_data = train_data
-        self.train_labels = train_labels
-        self.__initialize_centers()
-        new_centers = self.__update_clusters()
-        while self.__is_outside_of_threshold(new_centers):  # update until change is small
-            print("updating", time.time())
-            self.centers = new_centers
-            new_centers = self.__update_clusters()
+    def accuracy(self):
+        clusters = self.__get_clusters()
+        accuracy = 0
+        for cluster in clusters:
+            cluster_labels = self.labels[cluster]
+            # find what percent of the labels in the cluster are the vote
+            label_homogeneity = (cluster_labels == st.mode(cluster_labels)).sum() / len(cluster_labels)
+            accuracy += label_homogeneity / self.num_centers
+        return accuracy
+
+    def train(self, data, labels):
+        self.data = data
+        self.labels = labels
+        self.__initialize_centroids()
+        new_centroids = self.__update_centroids()
+        # while self.__is_outside_of_threshold(new_centroids):  # update until change is small
+        #     self.centers = new_centroids
+        #     new_centroids = self.__update_centroids()
+        for _ in range(100):
+            self.centers = self.__update_centroids()
+
+
 
     
-    def __initialize_centers(self):
+    def __initialize_centroids(self):
         """
         initialize 10 random data points to be 
         the first 10 centers
         """
-        init_centers = np.random.randint(low=0, high=self.train_data.shape[0], size=self.num_centers)
-        self.centers = self.train_data[init_centers]
+        init_centers = np.random.randint(low=0, high=self.data.shape[0], size=self.num_centers)
+        self.centers = self.data[init_centers]
     
     def __get_clusters(self):
         """
         sorts indices of training data into clusters, returns a 2D array of clusters
         """
-        clusters = [[]] * 10
-        for i in range(len(self.train_data)):
+        clusters = []
+        for _ in range(self.num_centers):
+            clusters.append([])
+        for i in range(len(self.data)):
             closest_center = 0
-            min_distance = np.linalg.norm(self.train_data[i] - self.centers[0], ord=2)
-            for j in range(self.num_centers):
-                distance = np.linalg.norm(self.train_data[i] - self.centers[j], ord=2)
+            min_distance = np.linalg.norm(self.data[i] - self.centers[0], ord=2)
+            for j in range(1, self.num_centers):
+                distance = np.linalg.norm(self.data[i] - self.centers[j], ord=2)
                 if distance < min_distance:
                     min_distance = distance
                     closest_center = j
-            clusters[closest_center].append(i)
-        return np.array(clusters)  # convert to numpy array
+            clusters[closest_center].append(i) 
+        clusters = np.array([np.array(cluster) for cluster in clusters], dtype='object')  # convert to numpy array
+        return clusters 
     
     def __get_centroid(self, cluster):
-        length, dim = cluster.shape
-        return np.array([np.sum(cluster[:, i])/length for i in range(dim)])
+        centroid = np.average(self.data[cluster], axis=0) # column averages
+        return centroid
 
-    def __update_clusters(self):
+    def __update_centroids(self):
         clusters = self.__get_clusters()
         new_centers = [0] * self.num_centers
         for i in range(self.num_centers):
-            new_centers[i] = self.__get_centroid(clusters[i]) # TODO: CHECK TO MAKE SURE THIS IS CORRECT
-        return new_centers
+            new_centers[i] = self.__get_centroid(clusters[i])
+        return np.array(new_centers)
     
     def __is_outside_of_threshold(self, new_centers):
         # check that update change is greater than threshold
-        delta = np.subtract(np.array(new_centers), self.centers)
+        delta = np.subtract(new_centers, self.centers)
         if np.average(delta) > self.threshold:
             return True
         return False
-
 
 
 if __name__ == "__main__":
@@ -75,3 +97,4 @@ if __name__ == "__main__":
     labels_test = labels_test[inds]
     model = kMeans()
     model.train(images_train, labels_train)
+    print(model.accuracy())
